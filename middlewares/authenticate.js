dd0b20be6bf9b1be7f7b0731940f56740d673275
viktor-harmai <vicg907@gmail.com>
2023-01-15 11:25:02 +0200

    fix auth

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -5,9 +5,9 @@ const { SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
   const { authorization = '' } = req.headers;
-  const [bearer, token] = authorization.split(' ');
-  if (bearer !== 'Bearer') {
-    next(Unauthorized('Not authorized'));
+  const [bearer = '', token = ''] = authorization.split(' ');
+  if (bearer !== 'Bearer' || token === '') {
+    next(Unauthorized('Not authorized1'));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
