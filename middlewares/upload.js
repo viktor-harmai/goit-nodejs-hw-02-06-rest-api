93a6226e89db4697d5e0023c4a79f8efb626de51
viktor-harmai <vicg907@gmail.com>
2023-01-16 22:48:57 +0200

    step-4

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,6 +1,19 @@
 const multer = require('multer');
+const path = require('path');
 
-const multerConfig = multer.diskStorage({});
+const tempDir = path.join(__dirname, '../', 'temp');
+
+const multerConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, tempDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+  limits: {
+    fileSize: 2048,
+  },
+});
 
 const upload = multer({
   storage: multerConfig,
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { validation, ctrlWrapper, authenticate } = require('../../middlewares');
+const { validation, ctrlWrapper, authenticate, upload } = require('../../middlewares');
 const { schemas } = require('../../models');
 const { auth: ctrl } = require('../../controllers');
 
@@ -14,6 +14,6 @@ router.get('/current', authenticate, ctrlWrapper(ctrl.getCurrent));
 
 router.post('/logout', authenticate, ctrlWrapper(ctrl.logout));
 
-router.patch('/avatars');
+router.patch('/avatars', authenticate, upload.single('avatar'), ctrlWrapper(ctrl.updateAvatar));
 
 module.exports = router;
